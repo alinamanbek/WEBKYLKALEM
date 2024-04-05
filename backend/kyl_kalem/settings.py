11af510e1bd3cc243349a2540352ab730ff7f61f
alinrmkh <amanbekovaalina10@gmail.com>
2024-04-05 22:41:40 +0600

    frontend

--- backend/kyl_kalem/settings.py
+++ backend/kyl_kalem/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,8 +62,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'kyl_kalem.urls'
-CORS_ALLOW_ALL_ORIGINS = True
-
+ 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Your React frontend URL
+]
 
 TEMPLATES = [
     {
--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,14 +1,17 @@
-'''from django.urls import path
-from . import views
 
-urlpatterns = [
-    path('register/', views.register_user),
-    path('login/', views.obtain_auth_token),
-]'''
 from django.urls import path
-from .views import register, login
+from .views import register, login,reset_password, reset_password_confirm,forgot_password
 
 urlpatterns = [
     path('api/register/', register, name='register'),
     path('api/login/', login, name='login'),
+    path('reset-password/', reset_password, name='reset_password'),
+    path('reset-password/<uidb64>/<token>/', reset_password_confirm, name='reset_password_confirm'),
+    path('api/forgot-password/', forgot_password, name='forgot_password'),  # Add this line for the forgot password endpoint
+
+ 
 ]
+ 
+
+ 
+  
